dafd614e013ec35113bdb5698353e8adc8eb7c6b
Mehran <mehran.git@hotmail.com>
2020-05-01 13:45:03 -0400

    add complementary task to Q4

--- Pile.java
+++ Pile.java
@@ -1,10 +1,10 @@
 
-public class Pile< E > {
+public class Pile<E> {
     private class Chainon {
         public E valeur;
         public Chainon precedant;
 
-        public Chainon( E valeur, Chainon precedant ) {
+        public Chainon(E valeur, Chainon precedant) {
             this.valeur = valeur;
             this.precedant = precedant;
         }
@@ -25,23 +25,25 @@ public class Pile< E > {
      * ************************************************************************
      * Placez vos explications ici :
      *
+     * au début j'ai utiliser "if", si pile est vide ou il a seulement une valeur alors la méthod
+     * ne fait rien. si il a plus que une valeur alors :
      *
+     * Avec le code "Chainon temp = sommet; "J'ai créer une variable objet local
+     * qui référe au premier élément(comme sommet).
      *
+     * puis avec le code "sommet = sommet.precedant;" sommet référe au deuxiem élément.
      *
+     * puis avec le code "temp.precedant = sommet.precedant;" le premier élément va référer
+     * Au Au troisième élément ou null (si il n'y a pas troisième élément).
      *
-     *
-     *
-     *
-     *
-     *
+     * puis avec le code "sommet.precedant = temp;" le deuxième élément référe au premier élément.
      * ************************************************************************
-     *
      */
     public void echanger() {
         // Placez votre code ici pour la question 4.
-        if(sommet != null && sommet.precedant !=  null){
+        if (sommet != null && sommet.precedant != null) {
             Chainon temp = sommet;
-            sommet=sommet.precedant;
+            sommet = sommet.precedant;
             temp.precedant = sommet.precedant;
             sommet.precedant = temp;
         }
@@ -52,20 +54,20 @@ public class Pile< E > {
     }
 
     public E sommet() throws ExceptionPileVide {
-        if( null == sommet ) {
-            throw new ExceptionPileVide( "sommet" );
+        if (null == sommet) {
+            throw new ExceptionPileVide("sommet");
         }
 
         return sommet.valeur;
     }
 
-    public void empiler( E valeur ) {
-        sommet = new Chainon( valeur, sommet );
+    public void empiler(E valeur) {
+        sommet = new Chainon(valeur, sommet);
     }
 
     public E depiler() throws ExceptionPileVide {
-        if( null == sommet ) {
-            throw new ExceptionPileVide( "depiler" );
+        if (null == sommet) {
+            throw new ExceptionPileVide("depiler");
         }
 
         E temp = sommet.valeur;
